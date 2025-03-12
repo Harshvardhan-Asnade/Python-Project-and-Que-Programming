ca32284491b1205111597469e9bcc0853d3742aa
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-03-12 13:16:23 +0530

    Operators

---
+++ Basic Python/06_Operator.py
@@ -0,0 +1,38 @@
+# Arithmetic Operators
+a = 7
+b = 4
+sum = a + b
+print(sum)
+
+# Assignment Operators
+a = 4-2  
+print(a)
+b = 6
+# b += 3 # Increment 
+b -= 3 # Decrement  
+print(b)
+
+# Comparison Operators
+
+d = 5==5
+
+print(d)
+
+
+# Logical Operators
+
+e = True or False
+
+# Truth table of 'or' 
+print("True or False is ", True or False)
+print("True or True is ", True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
+
+# Truth table of 'and' 
+print("True and False is ", True and False)
+print("True and True is ", True and True)
+print("False and True is ", False and True)
+print("False and False is ", False and False)
+
+print(not(True))
