160fb5386079744190f582f712a9fb6d0524c029
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-03-13 22:04:02 +0530

    Problem

---
+++ Problem6.py
@@ -0,0 +1,10 @@
+# Use comparison operator to find out whether ‘a’ given variable a is greater than
+# ‘b’ or not. Take a = 34 and b = 80    
+
+a = 80
+b = 32
+print("a is greater then b is " ,a>b)
+a1 = 32
+b2 = 80
+print("a is greater then b is " ,a1>b2)
+ 
