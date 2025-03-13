b2714d265a6c5390f047cb8077239afc657473ac
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-03-13 13:04:27 +0530

    Typecasting

---
+++ Basic Python/07_Typecasting.py
@@ -0,0 +1,25 @@
+# String to Float
+a = "31.2"
+b = float(a)
+t = type(b)
+print(t)  # <class 'float'>
+
+# String to Integer
+c = "10"
+d = int(c)
+t = type(d)
+print(t)  # <class 'int'>
+
+# Integer to Float
+e = 42
+f = float(e)
+t = type(f)
+print(t)  # <class 'float'>
+
+# Float to Integer
+g = 19.99
+h = int(g)
+t = type(h)
+print(t)  # <class 'int'>
+
+ 
