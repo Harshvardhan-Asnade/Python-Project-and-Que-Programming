a1ad897fe0f6b91968c598d101eb965bc3de96ea
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-03-13 22:04:11 +0530

    Problem

---
+++ Problem7.py
@@ -0,0 +1,4 @@
+# Write a python program to find an average of two numbers entered by the user.
+a=int(input("Enter the value of a : "))
+b=int(input("Enter the value of a : "))
+print("The average of two numbers is",(a+b)/2)
---
+++ Problem8.py
@@ -0,0 +1,2 @@
+a=int(input("Enter the value of a : "))
+print("The Square of two number is ",a*a)
