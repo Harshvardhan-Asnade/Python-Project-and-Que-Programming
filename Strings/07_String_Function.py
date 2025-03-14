df5731f0ddc661399b919da9830ee16e46ce47aa
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-03-14 22:35:50 +0530

    String

---
+++ Strings/07_String_Function.py
@@ -0,0 +1,9 @@
+name = "Hello World"
+
+print(len(name))
+print(name.endswith("ld"))
+print(name.startswith("ha"))
+print(name.capitalize())
+print(name.replace("o","e"))
+print(name.count("o"))
+print(name.find("o")) 
