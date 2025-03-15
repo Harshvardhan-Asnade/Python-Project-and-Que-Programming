508411f9ba6a20b37c33e0d0805a35a1d4f31a1b
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-03-15 13:11:10 +0530

    Problem

---
+++ Problem13.py
@@ -0,0 +1,8 @@
+#WAP to ask user for fav movie and stor them in a list
+movie1 = input("Enter Movie 1: ")
+movie2 = input("Enter Movie 2: ")
+movie3 = input("Enter Movie 3: ")
+
+movies = [movie1, movie2, movie3]
+
+print("Your favorite movies are:", movies)
