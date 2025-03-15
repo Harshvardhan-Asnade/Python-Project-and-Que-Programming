6d3350d3c75581f2a9a86f4bdc26c11c28fe94fa
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-03-15 22:03:28 +0530

    Set POP

---
+++ Dictionary & Set/07_Set_Pop.py
@@ -0,0 +1,14 @@
+ 
+set = {
+    "C", "C++", "Python", "Java", "CSS", "HTML", "Rust",
+    "JavaScript", "TypeScript", "Go", "Swift", "Kotlin",
+    "PHP", "Ruby", "Perl", "Dart", "Scala", "R", "Lua"
+}
+
+print(set.pop())
+print(set.pop())
+print(set.pop())
+print(set.pop())
+print(set.pop())
+print(set.pop())
+print(set.pop())
