1533237a552e47f0b3aff1c87969162cbafbd804
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-03-15 18:20:20 +0530

    Dictionary

---
+++ Dictionary & Set/01_ Dictionary.py
@@ -0,0 +1,12 @@
+info = {
+    "name": "Harsh",  
+    "subje]"
+    "age": 18,  
+    "height": 5.8,  
+    "married": False,  
+    "hobbies": ["gaming", "searching about tech"],  
+    "contact_numbers": ("9876543210", "9123456789"),  
+    "skills": {"Python", "AI", "C++"},  
+    "blood_type": "B+",  
+    }  
+print(info)
