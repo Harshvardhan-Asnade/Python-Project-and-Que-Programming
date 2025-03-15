f22dae808523d29262916c063e51475f2158c1f9
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-03-15 18:29:34 +0530

    Nested Dictionary

---
+++ Dictionary & Set/02_Nested_Dictionary.py
@@ -0,0 +1,17 @@
+info = {
+    "name": "Harsh",  
+    "skills": {
+        "C": "Basic",
+        "C++": "DSA",
+        "WEBDev": "MERNSTACK",
+        "Python": "DSA",
+    },   
+    "age": 18,  
+    "height": 5.8,  
+    "married": False,  
+    "hobbies": ["gaming", "searching about tech"],  
+    "contact_numbers": ("9876543210", "9123456789"),  
+    "blood_type": "B+",  
+}  
+print(info)
+print(info["skills"])
